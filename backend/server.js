a72700d85af864fd92e0e7ae9fdf573bc05e53ac
anushruti11 <anushruti1132001@gmail.com>
2022-07-27 01:52:44 +0530

    User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,50 @@
-const express = require("express");
-const notes = require("./data/notes");
-const dotenv = require('dotenv');
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import path from "path";
 
-const app = express();
+import noteRoutes from "./routes/noteRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
+connectDB();
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+const app = express(); // main thing
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
+app.use(express.json()); // to accept json data
 
-  res.send(note);
-});
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
+
+// --------------------------deployment------------------------------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+      .bold
+  )
+);
