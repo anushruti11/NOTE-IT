60f75a46b636aade9eb68f90062c8dd192e90156
anushruti11 <anushruti1132001@gmail.com>
2022-07-27 23:33:27 +0530

    frontend

--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -35,9 +35,9 @@ function RegisterScreen({ history }) {
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "notezipper");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("upload_preset", "noteit");
+      data.append("cloud_name", "anushruti");
+      fetch("https://api.cloudinary.com/v1_1/anushruti/image/upload", {
         method: "post",
         body: data,
       })
